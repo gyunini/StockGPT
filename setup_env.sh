415a5b240038daea923b798187a1e8150f0d3929
Gyun Lee <gyunini@gmail.com>
2025-02-08 22:11:31 +0900

    add .sh file

---
+++ setup_env.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# 에러 발생 시 즉시 종료
+set -e
+
+
+pip install --user pipx
+pipx install uv
+uv -V
+uv venv pytorch
+
+echo "가상환경 활성화"
+source pytorch/bin/activate
+
+echo "pyproject.toml의 패키지 설치"
+uv pip install --requirements pyproject.toml
+
+echo "CUDA 지원 여부 확인"
+python -c "import torch; print(torch.cuda.is_available())"
+
+echo "모든 작업 완료"
